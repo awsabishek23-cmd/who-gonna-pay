d12403ef2ddc587bd056809bf1e15c950f309047
awsabishek <awsabishek23@gmail.com>
2026-02-23 07:22:36 +0530

    coolvetica bold to light

--- src/App.svelte
+++ src/App.svelte
@@ -16,15 +16,20 @@
       payer = users[Math.round(Math.random() * (users.length - 1))]
       resultOpposite = !resultOpposite
     }
-    
+    const handleToDark = () => {
+  
+    }
 </script>
 
-<div class="flex justify-center">
+<div class="flex justify-center h-screen ">
   <div class="container max-w-5xl px-3 mt-9">
     <!-- svelte-ignore a11y_consider_explicit_label -->
       {#if resultOpposite}
         <div>
-          <p class="text-center text-3xl">who gonna pay!</p>
+          <div class="flex items-center justify-between">
+            <p class="text-3xl pb-3">who gonna pay!</p>
+            <button class="mb-1 cursor-pointer" onclick={handleToDark}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"/></svg></button>
+          </div>
           <form onsubmit={handleSubmit}>
               <input type="text" bind:value={user} class="border-b-2 outline-none w-full text-2xl" placeholder="Enter people"><br>
               <div class="flex justify-end-safe">
