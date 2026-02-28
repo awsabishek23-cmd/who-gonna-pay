49087c7058af6045e2f7f48cfe1a84c99d395da0
awsabishek <awsabishek23@gmail.com>
2026-02-28 17:30:44 +0530

    accessibility added in submit and changed placeholder

--- src/App.svelte
+++ src/App.svelte
@@ -73,10 +73,10 @@
             <p class="text-3xl pb-3">who gonna pay!</p>
           </div>
           <form onsubmit={handleSubmit}>
-              <input type="text" bind:value={user} class="border-b-2 outline-none w-full text-2xl" placeholder="Enter people"><br>
+              <input type="text" bind:value={user} class="border-b-2 outline-none w-full text-2xl mb-2" placeholder="Enter a name"><br>
               <div class="flex justify-end-safe">
-                <button class="cursor-pointer mt-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M720-400v-120H600v-80h120v-120h80v120h120v80H800v120h-80ZM247-527q-47-47-47-113t47-113q47-47 113-47t113 47q47 47 47 113t-47 113q-47 47-113 47t-113-47ZM40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm80-80h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm296.5-343.5Q440-607 440-640t-23.5-56.5Q393-720 360-720t-56.5 23.5Q280-673 280-640t23.5 56.5Q327-560 360-560t56.5-23.5ZM360-640Zm0 400Z"/></svg>
+                <button class="cursor-pointer">
+                  <p class="cursor-pointer bg-black text-white  px-5 py-1 rounded-full">Submit</p>
                 </button>
               </div>
           </form>
@@ -86,7 +86,7 @@
             <div class="flex justify-between items-center pt-5 px-0.75">
               <p class="text-2xl">{user}</p>
               <button onclick={() => handleDelete(index)} class="cursor-pointer">
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
               </button>
             </div>
           {/each}
